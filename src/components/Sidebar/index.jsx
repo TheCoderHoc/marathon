30322ddc0c545cd399509078789717b4dbfda3ca
Daviloy <ubakawilson@gmail.com>
2023-03-23 17:34:42 +0100

    added a login button in the sidebar for when the user is not logged in

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,26 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
-import { Avatar, Divider } from "antd";
+import { Avatar, Divider, Button } from "antd";
+import { BsMicrosoft } from "react-icons/bs";
 import { VscSearch } from "react-icons/vsc";
 import userPic from "../../assets/images/user.png";
 
 const Sidebar = () => {
+    const [user, setUser] = useState(null);
+
     return (
         <aside className="sidebar">
             <div className="sidebar-top-content">
-                <div className="sidebar-user">
-                    <div className="sidebar-user-image">
-                        <Avatar
-                            src={userPic}
-                            size="large"
-                            alt="User Profile Photo"
-                        />
+                {user ? (
+                    <div className="sidebar-user">
+                        <div className="sidebar-user-image">
+                            <Avatar
+                                src={userPic}
+                                size="large"
+                                alt="User Profile Photo"
+                            />
+                        </div>
+                        <div className="sidebar-user-info">
+                            <h3 className="sidebar-user-name">Dave Wilson</h3>
+                            <p className="sidebar-user-status">
+                                You're offline...
+                            </p>
+                        </div>
                     </div>
-                    <div className="sidebar-user-info">
-                        <h3 className="sidebar-user-name">Dave Wilson</h3>
-                        <p className="sidebar-user-status">You're offline...</p>
-                    </div>
-                </div>
+                ) : (
+                    <Button
+                        type="primary"
+                        icon={<BsMicrosoft />}
+                        block
+                        className="sidebar-login-btn"
+                        onClick={() => console.log("Coming soon...")}
+                    >
+                        Login Microsoft
+                    </Button>
+                )}
 
                 <form className="sidebar-search-form">
                     <input
