667baf9585d0946066646090983efecfe26510b0
Daviloy <ubakawilson@gmail.com>
2023-03-23 09:38:48 +0100

    set up application routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import "./App.css";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 const App = () => {
     return (
-        <>
-            <h1>Marathon</h1>
-        </>
+        <Routes>
+            <Route path="/" element={<Navigate to="tasks" />} />
+            <Route path="tasks" element={<h1>All Tasks</h1>} />
+            <Route path="tasks/:list" element={<h1>Filtered Tasks</h1>} />
+            <Route path="settings" element={<h1>Settings</h1>} />
+        </Routes>
     );
 };
 
